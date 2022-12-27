c9de5b909a586302fc3e98d87398dc22e5fedf96
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-27 19:29:01 +0200

    step4

--- index.js
+++ index.js
@@ -15,6 +15,25 @@ app.get("/time", (req, res) => {
   res.send({ status: 200, message: `${now.getHours()}:${now.getMinutes()}` });
 });
 
+app.get("/hello/:ID", (req, res) => {
+  res.send({ status: 200, message: `Hello ${req.params.ID}` });
+});
+
+app.get("/search", (req, res) => {
+  const queryData = req.query.s;
+
+  let response = {
+    status: 500,
+    error: true,
+    message: "You have to provide search",
+  };
+  if (queryData != null) {
+    response = { status: 200, message: "ok", data: req.query.s };
+  }
+
+  res.send(response);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
