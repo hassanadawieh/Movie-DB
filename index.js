47f586ded2a28e0ab89377d247b314daa911ad97
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-27 19:17:06 +0200

    Step3

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  const now = new Date();
+  res.send({ status: 200, message: `${now.getHours()}:${now.getMinutes()}` });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
