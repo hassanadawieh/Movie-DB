66ef08747f7c5c03dc5f81fa86f3d85c9c9ed0b0
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-27 19:39:42 +0200

    Step 5

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب", year: 1992, rating: 6.2 },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -32,6 +39,25 @@ app.get("/search", (req, res) => {
   }
 
   res.send(response);
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Step 5
+
+app.get("/movies/create", (req, res) => {
+  res.send({ status: 200, message: "Temporary" });
+});
+
+app.get("/movies/read", (req, res) => {
+  res.send({ status: 200, data: JSON.stringify(movies) });
+});
+
+app.get("/movies/update", (req, res) => {
+  res.send({ status: 200, message: "Temporary" });
+});
+
+app.get("/movies/delete", (req, res) => {
+  res.send({ status: 200, message: "Temporary" });
 });
 
 app.listen(port, () => {
